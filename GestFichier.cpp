02435de9a70a8d39452b4b516280bb8e5c4e38ec
Antoine <obitonio07@yahoo.fr>
2017-12-03 21:22:34 +0100

    Ajout de la doc et des metadonnées

--- GestFichier.cpp
+++ GestFichier.cpp
@@ -21,15 +21,20 @@ bool GestFichier::sauvegarder (string url, TypeTrajet typeTrajet)
 	if (fic)
 	{
 		fic.clear();
-
+		string texteAEcrire = "";
 		streambuf *oldCoutBuffer = cout.rdbuf(fic.rdbuf());
-
-		EcrireMetaDonnees();
+		int nbTrajets = 0;
 
 		for (int i=0; i<m_collection.GetLongueur(); i++)
 		{
-			EcrireTrajet(m_collection.GetTrajet(i), typeTrajet);
+			if (EcrireTrajet(m_collection.GetTrajet(i), texteAEcrire, typeTrajet))
+				nbTrajets++;
 		}
+
+		CritereSauvegarde critereSauvegarde = (typeTrajet == TypeTrajet::DEUX)? CritereSauvegarde::COMPLETE : CritereSauvegarde::TYPE_TRAJETS;
+
+		EcrireMetaDonnees(nbTrajets, critereSauvegarde);
+		cout << texteAEcrire;
 
 		cout.rdbuf(oldCoutBuffer);
 		fic.close();
@@ -48,9 +53,9 @@ bool GestFichier::sauvegarder (string url, string villeDepart, string villeArriv
 	{
 		fic.clear();
 
+		string texteAEcrire = "";
 		streambuf *oldCoutBuffer = cout.rdbuf(fic.rdbuf());
-
-		EcrireMetaDonnees();
+		int nbTrajets = 0;
 
 		for (int i=0; i<m_collection.GetLongueur(); i++)
 		{
@@ -62,12 +67,14 @@ bool GestFichier::sauvegarder (string url, string villeDepart, string villeArriv
 				if ( !villeArrivee.empty() ) // Vérif si ET ville arrivée
 				{
 					if ( villeArrivee.compare(trajetCourant->GetVilleB()) == 0 )
-						EcrireTrajet(trajetCourant);
+						EcrireTrajet(trajetCourant, texteAEcrire);
 				}
 				else
 				{
-					EcrireTrajet(trajetCourant);
+					EcrireTrajet(trajetCourant, texteAEcrire);
 				}
+
+				nbTrajets++;
 			}
 			else
 			{
@@ -77,15 +84,20 @@ bool GestFichier::sauvegarder (string url, string villeDepart, string villeArriv
 					if ( !villeDepart.empty() ) // Verif di ET ville départ
 					{
 						if ( villeDepart.compare(trajetCourant->GetVilleA()) == 0 )
-							EcrireTrajet(trajetCourant);
+							EcrireTrajet(trajetCourant, texteAEcrire);
 					}
 					else
 					{
-						EcrireTrajet(trajetCourant);
+						EcrireTrajet(trajetCourant, texteAEcrire);
 					}
+
+					nbTrajets++;
 				}
 			}
 		}
+
+		EcrireMetaDonnees(nbTrajets, CritereSauvegarde::VILLE);
+		cout << texteAEcrire;
 
 		cout.rdbuf(oldCoutBuffer);
 		fic.close();
@@ -104,15 +116,21 @@ bool GestFichier::sauvegarder (string url, int debutIntervalle, int finIntervall
 	{
 		fic.clear();
 
+		string texteAEcrire = "";
 		streambuf *oldCoutBuffer = cout.rdbuf(fic.rdbuf());
-
-		EcrireMetaDonnees();
+		int nbTrajets = 0;
 
 		for (int i=0; i<m_collection.GetLongueur(); i++)
 		{
 			if ( (i + 1 ) >= debutIntervalle && (i + 1) <= finIntervalle)
-				EcrireTrajet(m_collection.GetTrajet(i));
+			{
+				EcrireTrajet(m_collection.GetTrajet(i), texteAEcrire);
+				nbTrajets++;
+			}
 		}
+
+		EcrireMetaDonnees(nbTrajets, CritereSauvegarde::INTERVALLE);
+		cout << texteAEcrire;
 
 		cout.rdbuf(oldCoutBuffer);
 		fic.close();
@@ -127,45 +145,52 @@ GestFichier::~GestFichier()
 {
 }
 
-void GestFichier::EcrireTrajetSimple(TrajetSimple *unTrajetSimple)
+void GestFichier::EcrireTrajetSimple(TrajetSimple *unTrajetSimple, string &texteAEcrire)
 {
-	cout << "VilleDepart: " << unTrajetSimple->GetVilleA() << endl;
-	cout << "VilleArrivee: " << unTrajetSimple->GetVilleB() << endl;
-	cout << "MoyenTansport: " << lesMoyensTransport[unTrajetSimple->GetMoyenTransport()] << endl;
+	texteAEcrire += "VilleDepart: " + string( unTrajetSimple->GetVilleA() ) + "\n";
+	texteAEcrire += "VilleArrivee: " + string( unTrajetSimple->GetVilleB() ) + "\n";
+	texteAEcrire += "MoyenTansport: " + string( lesMoyensTransport[unTrajetSimple->GetMoyenTransport()] ) + "\n";
 }
 
-void GestFichier::EcrireTrajet (Trajet *unTrajet, TypeTrajet typeTrajet)
+bool GestFichier::EcrireTrajet (Trajet *unTrajet, string &texteAEcrire, TypeTrajet typeTrajet)
 {
+	bool resultat = false;
 	TrajetSimple *trajetSimple = dynamic_cast<TrajetSimple*> (unTrajet);
 
 	if (trajetSimple != nullptr && (typeTrajet == TypeTrajet::SIMPLE || typeTrajet == TypeTrajet::DEUX) )
 	{
-		cout << "=====" << endl;
-		EcrireTrajetSimple(trajetSimple);
-		cout << endl;
+		texteAEcrire += "=====\n";
+		EcrireTrajetSimple(trajetSimple, texteAEcrire);
+		texteAEcrire += "\n";
+
+		resultat = true;
 	}
 	else
 	{
 		TrajetCompose *trajetCompose = dynamic_cast<TrajetCompose*> (unTrajet);
 		if (trajetCompose != nullptr && (typeTrajet == TypeTrajet::COMPOSE || typeTrajet == TypeTrajet::DEUX))
 		{
-			cout << "=====" << endl;
+			texteAEcrire += "=====\n";
 			for (int y=0; y<trajetCompose->GetLesTrajets()->GetLongueur(); y++)
 			{
 				TrajetSimple *tmp = dynamic_cast<TrajetSimple*> ( trajetCompose->GetLesTrajets()->GetTrajet(y) );
-				EcrireTrajetSimple(tmp);
+				EcrireTrajetSimple(tmp, texteAEcrire);
 			}
-			cout << endl;
+			texteAEcrire += "\n";
+
+			resultat = true;
 		}
 	}
+
+	return resultat;
 }
 
-void GestFichier::EcrireMetaDonnees()
+void GestFichier::EcrireMetaDonnees(const int nbTrajets, const CritereSauvegarde critereSauvegarde)
 {
 	time_t t = time( 0 );
 
-	cout << "Nombre de trajets: " << m_collection.GetLongueur() << endl;
-	cout << "Critère de sauvegarde: complète" << endl;
-	cout << "Date: " << ctime( &t ) << endl;
+	cout << "Nombre de trajets: " << nbTrajets << "\n";
+	cout << "Critère de sauvegarde: " << lesCriteresSauvegarde[critereSauvegarde] << "\n";
+	cout << "Date: " << ctime( &t ) << "\n";
 }
 
--- GestFichier.h
+++ GestFichier.h
@@ -19,29 +19,81 @@ enum TypeTrajet
 	SIMPLE, COMPOSE, DEUX
 };
 
+enum CritereSauvegarde
+{
+	COMPLETE, TYPE_TRAJETS, VILLE, INTERVALLE
+};
+
+const string lesCriteresSauvegarde[] = {"sans critère de selection", "selon le type des trajets", "selon la ville de départ et / ou d'arrivée", "selon une selection de trajets"};
+
 class GestFichier
 {
 	public:
 		GestFichier(Collection &collection);
+		// Mode d'emploi :
+		// 	Constructeur de la classe GestFichier.
+		// 	Initialise la collection sur laquelle les fonctions de la classe seront amenées à travailler.
+		// Paramètres :
+		// 	collection : la collection à sauvegarder pour effectuer les traitements de savegarde / chargement
 
 		bool sauvegarder (string url, TypeTrajet typeTrajet = TypeTrajet::DEUX);
+		// Mode d'emploi :
+		// 	Cette méthode permet de sauvegarder la collection de trajets (initialisée dans le constructeur) dans le fichier spécifié en paramètre.
+		// 	Il est possible de choisir le type des trajets à sauvegarder (par défaut les deux) via le paramètre typeTrajet.
+		// 	Si existant le fichier est vidé pour être remplacé par la sauvegarde.
+		// Paramètres :
+		// 	url : nom du fichier de sauvegarde
+		// 	typeTrajet : type des trajets à sauvegarder, par défaut les deux
+
 
 		bool sauvegarder (string url, string villeDepart, string villeArrivee);
-		// Spécifier une ville de départ et / ou d'arrivée, pour un ou mettre la ville non desirée en null
+		// Mode d'emploi :
+		//	Cette méthode permet de sauvegarder la collection de trajets (initialisée dans le constructeur) dans le fichier spécifié en paramètre.
+		//	Les trajets sauvegardés respecteront la ville de départ et / ou la ville d'arrivée fournit en paramètres.
+		// Paramètres :
+		//	url : nom du fichier de sauvegarde
+		//	villeDepart : ville de départ que les trajets à sauvegarder doivent respecter ou "" pour s'abstenir du paramètre
+		//	villeArrivee : ville de d'arrivée que les trajets à sauvegardrr doivent respecter ou "" pour s'abstenir du paramètre
 
 		bool sauvegarder (string url, int debutIntervalle, int finIntervalle);
+		// Mode d'emploi :
+		//	Cette méthode permet de sauvegarder la collection de trajets (initialisée dans le constructeur) dans le fichier spécifié en paramètre.
+		//	La sauvegarde s'effectue en respectant l'intervalle donné, du trajet n au trajet m.
+		// Paramètres :
+		//	url : nom du fichier de sauvegarde
+		//	debutIntervalle : début n de l'intervalle
+		//	finIntervalle : fin m de l'intervalle
 
 		virtual ~GestFichier();
+		// Mode d'emploi :
+		//	Destructeur de la classe
 
 	private:
-		void EcrireTrajetSimple (TrajetSimple *unTrajetSimple);
+		void EcrireTrajetSimple (TrajetSimple *unTrajetSimple, string &texteAEcrire);
+		// Mode d'emploi :
+		//	Ecrit la ville de départ et d'arrivé ainsi que le moyen de transport d'un trajet sur la référence donnée en paramètre.
+		// Paramètres :
+		//	unTrajetSimple : le trajet à écrire
+		//	texteAEcrire : le texte sur lequel ajouté le trajet
 
-		void EcrireTrajet (Trajet *unTrajet, TypeTrajet typeTrajet = TypeTrajet::DEUX);
+		bool EcrireTrajet (Trajet *unTrajet, string &texteAEcrire, TypeTrajet typeTrajet = TypeTrajet::DEUX);
+		// Mode d'emploi :
+		//	Determine si le trajet est simple ou composé puis l'écrit ou non en fonction du type de trajet à écrire
+		// Paramètres :
+		//	unTrajet : le trajet à traiter
+		//	texteAEcrire : le texte sur lequel le trajet sera écrit
+		//	typeTrajet : définit le type de trajet autorisé à etre écrit
 
-		void EcrireMetaDonnees();
+
+		void EcrireMetaDonnees(const int nbTrajets, const CritereSauvegarde critereSauvegarde);
+		// Mode d'emploi :
+		//	Ecrit les métadonnées dans l'entete du fichier de sauvegarde
+		// Paramètres :
+		//	nbTrajets : le nombre de trajet à indiquer en metadonnées
+		//	critereSauvegarde : le critere de sauvegarde à indiquer en metadonnées
 
 	private:
-		Collection &m_collection;
+		Collection &m_collection; // Collection de trajets à partir de laquelle effectuer les sauvegardes / restauration
 };
 
 
